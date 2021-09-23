49f0866a581710e168ea594bb03fd6bc758de251
CheetoDustNeo <misterrolypoly@gmail.com>
2021-09-23 04:00:25 -0500

    changed route handling

--- src/app.js
+++ src/app.js
@@ -1,29 +1,13 @@
 const express = require('express')
+const path = require('path')
 //------------------------------------------------------------------------
+const publicDirectoryPath = path.join(__dirname, '../public')
+
 
 const app = express()
-app.get('', (req, res) => {
-    res.send('<h1>Joefie, stranger</h1>')
-})
+app.use(express.static(publicDirectoryPath));
 
-app.get('/about', (req, res) => {
-    res.send('You c\'n learn all \'bout our town\'s history here, stranger.')
-})
 
-app.get('/help', (req, res) => {
-    //res.send('Even though yer a little strange, we help anyone we can, stranger.')
-    res.send( {
-        greeting: 'Even though yer a little strange, we help anyone we can, stranger.',
-        gifts: [{
-            item: 'Sturdy Flask',
-            description: 'A flask that seems rough hewn and dusty, but just holding it tells you that it will be a steadfast companion.'
-
-        }, {
-            item: 'Gunpowder',
-            description: 'Black powder with many versatile uses, beyond being a catalyst for harm.'
-        }]
-    })
-})
 
 app.get('/weather', (req, res) => {
     res.send('How\'s the skies lookin\', stranger?')
