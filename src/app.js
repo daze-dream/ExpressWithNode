b7563d2dda5e51141ce750e58215e2234377d401
CheetoDustNeo <misterrolypoly@gmail.com>
2021-09-24 15:38:02 -0500

    changed for handlebars routing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 //------------------------------------------------------------------------
-const publicDirectoryPath = path.join(__dirname, '../public')
-
-
 const app = express()
+//------------------------------------------------------------------------
+// express paths
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views' )
+const partialsPath = path.join(__dirname, '../templates/partials')
+//------------------------------------------------------------------------
+//handlebars setup
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+//------------------------------------------------------------------------
+//static directory
 app.use(express.static(publicDirectoryPath));
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Joefie, Stranger',
+        body: 'Anythin\' you need\'d be round these parts, stranger.'
+    }) 
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Memorial of the Deadeye, Center of Town',
+        body: 'You c\'n learn all \'bout our town\'s history here, stranger.'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Goodman\'s General Store', 
+        img: "img/joefie.jpg"
+    })
+})
 
 
 
