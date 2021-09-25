e53d2935810c56a8968ef3168d8d9674923427cd
CheetoDustNeo <misterrolypoly@gmail.com>
2021-09-25 01:06:01 -0500

    more practice

--- playground/es6_default_params.js
+++ playground/es6_default_params.js
@@ -1,1 +1,12 @@
 // A look at default params
+
+const greeter = (name = 'unnamed one') => {
+    console.log('Hello, ' + name)
+}
+
+greeter('stranger')
+
+greeter()
+
+
+// can even add default parameters to destructured paramters. 
