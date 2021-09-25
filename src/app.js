d24395868d4978b5166698221e62a02e9701a049
CheetoDustNeo <misterrolypoly@gmail.com>
2021-09-25 00:57:17 -0500

    integrate with weather forecast code

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 const { response } = require('express')
+const { uptime } = require('process')
+const forecast = require('./utils/forecast.js')
+const geocode = require('./utils/geocode.js')
 //------------------------------------------------------------------------
 const app = express()
 //------------------------------------------------------------------------
@@ -38,14 +41,65 @@ app.get('/help', (req, res) => {
     })
 })
 
-
-
 app.get('/weather', (req, res) => {
-    res.send('How\'s the skies lookin\', stranger?')
+    console.log(req.query)
+    if(!req.query.address) {
+        return res.send({
+            error: 'I need a location to scout for, stranger.'
+        })
+    }
+    finalForecast = 'So, ';
+    geocode.geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if(error)
+        {
+            return res.send({
+                errorCode: error
+            })
+        }
+        finalForecast = ""
+        console.log(latitude, longitude, location)
+        forecast.forecast( latitude, longitude, 'f', (error, {weatherDesc: weather, temperature, unit, feelsLike} = {}) => {
+            if(error)
+            {
+                return res.send(error)
+            }
+             finalForecast = 'In ' 
+                    + location
+                    + ', it is ' 
+                    + weather
+                    + ', with a temperature of '
+                    + temperature
+                    + unit
+                    + ' (feels like '
+                    + feelsLike
+                    +'). '
+                    res.send({
+                        forecast: finalForecast,
+                        location: 'Tchwatta Sand Dunes',
+                        address: req.query.address,
+                    })
+        })
+
+    })
+
 })
 
 app.get('/town_square', (req, res) => {
     res.send('Don\' look like you from \'round these parts. \n Your parts don\' look like they from \'round these parts.')
+})
+
+app.get('/inventory', (req, res) => {
+    //query is how we process incoming requests from the user
+    if(!req.query.type)
+    {
+        return res.send({
+            error: 'Must provide type of itme to search for'
+        })
+    }
+    console.log(req.query)
+    res.send({
+        items: []
+    })
 })
 
 app.get('/help/*', (req, res) => {
